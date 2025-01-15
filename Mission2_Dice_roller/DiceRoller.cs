3393171835244f190c1e5ee857cff00b4d4bb6ff
masondalton <mcoled@byu.edu>
2025-01-15 09:17:14 -0700

    Started program, classes and methods

---
+++ Mission2_Dice_roller/DiceRoller.cs
@@ -0,0 +1,11 @@
+namespace Mission2_Dice_roller;
+
+public class DiceRoller(int numberDiceRolls)
+{
+        // Pass back array of the results to print in main
+        // Method to do the die simulation
+        public static void twoDieRoll(int)
+        {
+                
+        }
+}
--- Mission2_Dice_roller/Program.cs
+++ Mission2_Dice_roller/Program.cs
@@ -4,8 +4,10 @@
    NOT a one 12-sided die.) Use an array to keep track of the number of times that each
    combination is thrown. In other words, keep track of how many times the combination of the two
    simulated dice is 2, how many times the combination is 3, and so on, all the way up through 12.
-   Allow the user to choose how many times the “dice” will be thrown. Then, once you have that
-   number of rolls, pass that number to a second class that has a method that simulates the roll of
+   
+   Allow the user to choose how many times the “dice” will be thrown. 
+   
+   Then, once you have that number of rolls, pass that number to a second class that has a method that simulates the roll of
    the dice for the number of times that the user specified. Store the results in an array. The method
    in the second class should return that array containing the roll results to the first class. In the first
    class, use that array to print a histogram (using the * character) that shows the total percentage
@@ -32,10 +34,32 @@
    NOTE: Due to rounding issues and the fact that you ca
  */
 
+using Mission2_Dice_roller;
+
 internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int rollsNum = 0;
+        
+        // Ask user for input for number of simulated dice rolls
+        Console.WriteLine("Welcome to the dice simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        
+        // Store user input for desired rolls
+        int.TryParse(Console.ReadLine(), out rollsNum);
+        
+        // for the number of times user said
+             // roll die 1 and collect result
+             // roll die 2 and collect
+             // add result
+             
+        // for each number of possible to be rolled
+            // print number (2, 3, 4, 5, ...)
+            // callculate perecentage of times it was rolled
+            // pringt an asterisk fo each percent it was rolled (loop but also method exists)
+        DiceRoller.twoDieRoll(rollsNum);
+
+
     }
 }
