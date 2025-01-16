a81925606eea6e3021863779f1f56220a79c9752
masondalton <mcoled@byu.edu>
2025-01-15 19:38:59 -0700

    Added comments

--- Mission2_Dice_roller/DiceRoller.cs
+++ Mission2_Dice_roller/DiceRoller.cs
@@ -2,13 +2,6 @@ namespace Mission2_Dice_roller;
 
 public class DiceRoller()
 {
-        // Pass back array of the results to print in main
-        // Method to do the die simulation
-        
-        // for the number of times user said
-        // roll die 1 and collect result
-        // roll die 2 and collect
-        // add result
         public static int[] twoDieRoll(int numOfRolls)
         {
                 // Declare variables needed for dice rolls
@@ -17,14 +10,17 @@ public class DiceRoller()
                 int dice1 = 0;
                 int dice2 = 0;
 
+                // For loop to roll die for how many attempt user requested
                 for (int i = 0; i < numOfRolls; i++)
                 {
                         dice1 = rnd.Next(1, 7);
                         dice2 = rnd.Next(1, 7);
-
+                        
+                        // Adds sum of both die to array 
                         rollSums[i] = (dice1 + dice2);
                 }
                 
+                // Returns sum array for creating histogram
                 return rollSums;
         }
 }
